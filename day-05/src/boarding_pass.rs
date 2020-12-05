c2369f8a1c8a64295bbf04b4a8d8d0d041b7842c
Vlad Stepanov <8uk.8ak@gmail.com>
2020-12-05 12:18:17 +0300

    [solution] day 5: both parts

---
+++ day-05/src/boarding_pass.rs
@@ -0,0 +1,67 @@
+use std::str::FromStr;
+
+#[derive(Debug)]
+pub(crate) struct BoardingPass {
+    row: u32,
+    seat: u32,
+}
+
+#[derive(Debug)]
+pub(crate) struct UnknownFormatError;
+
+impl FromStr for BoardingPass {
+    type Err = UnknownFormatError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let row =
+            s.chars()
+                .clone()
+                .take(7)
+                .enumerate()
+                .try_fold(0u32, |acc, (idx, chr)| match chr {
+                    'F' => Ok(acc),
+                    'B' => Ok(acc | 1 << (6 - idx)),
+                    _ => Err(UnknownFormatError),
+                })?;
+
+        let seat =
+            s.chars()
+                .skip(7)
+                .take(3)
+                .enumerate()
+                .try_fold(0u32, |acc, (idx, chr)| match chr {
+                    'L' => Ok(acc),
+                    'R' => Ok(acc | 1 << (2 - idx)),
+                    _ => Err(UnknownFormatError),
+                })?;
+
+        Ok(Self { row, seat })
+    }
+}
+
+impl BoardingPass {
+    pub(crate) fn id(&self) -> u32 {
+        self.row * 8 + self.seat
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_examples() {
+        macro_rules! assert_pass_id_eq {
+            ($pass: expr, $id: expr) => {
+                let pass: BoardingPass = $pass.parse().expect("invalid format");
+
+                assert_eq!(pass.id(), $id);
+            };
+        }
+
+        assert_pass_id_eq!("FBFBBFFRLR", 357);
+        assert_pass_id_eq!("BFFFBBFRRR", 567);
+        assert_pass_id_eq!("FFFBBBFRRR", 119);
+        assert_pass_id_eq!("BBFFBBFRLL", 820);
+    }
+}
---
+++ day-05/src/lib.rs
@@ -0,0 +1,52 @@
+use std::{convert::TryFrom, error::Error};
+
+use advent_utils::{parse_file, Part, Solver};
+
+mod boarding_pass;
+
+use boarding_pass::BoardingPass;
+
+#[derive(Debug)]
+pub struct Solution {
+    passes: Vec<BoardingPass>,
+}
+
+impl TryFrom<String> for Solution {
+    type Error = Box<dyn Error>;
+
+    fn try_from(path: String) -> Result<Self, Self::Error> {
+        let passes = parse_file(path)?;
+
+        Ok(Self { passes })
+    }
+}
+
+fn sum_up_to_n(n: u32) -> u32 {
+    (n * (n + 1)) >> 1
+}
+
+impl Solver for Solution {
+    fn solve(&self, part: Part) -> String {
+        let ids = self.passes.iter().map(BoardingPass::id).collect::<Vec<_>>();
+
+        let max_id = ids.iter().max().expect("no passes provided");
+
+        match part {
+            Part::One => {
+                format!("max ID is {}", max_id)
+            }
+            Part::Two => {
+                let min_id = ids.iter().min().expect("no passes provided");
+
+                let expected_sum = sum_up_to_n(*max_id) - sum_up_to_n(*min_id - 1);
+                let real_sum = ids.iter().sum::<u32>();
+
+                format!("missing pass ID is {}", expected_sum - real_sum)
+            }
+        }
+    }
+
+    fn day_number() -> u32 {
+        5
+    }
+}
