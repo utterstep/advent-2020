0a28ef20be3530f0c8c88e7045cf57e5b2b6596f
Vlad Stepanov <8uk.8ak@gmail.com>
2021-01-07 13:19:02 +0300

    [solution] day-14: part one

---
+++ day-14/src/lib.rs
@@ -0,0 +1,41 @@
+use std::{convert::TryFrom, error::Error, path::PathBuf};
+
+use advent_utils::{parse_file, Part, Solver};
+
+mod command;
+mod memory;
+
+use command::Command;
+use memory::Memory;
+
+#[derive(Debug)]
+pub struct Solution {
+    commands: Vec<Command>,
+}
+
+impl TryFrom<PathBuf> for Solution {
+    type Error = Box<dyn Error>;
+
+    fn try_from(input_file: PathBuf) -> Result<Self, Self::Error> {
+        Ok(Self { commands: parse_file(input_file)? })
+    }
+}
+
+impl Solver for Solution {
+    fn day_number() -> u32 {
+        14
+    }
+
+    fn solve(&self, part: Part) -> String {
+        let mut mem = Memory::new();
+
+        match part {
+            Part::One => {
+                self.commands.iter().for_each(|c| mem.process_command(c));
+
+                format!("sum of values in memory: {}", mem.data().values().sum::<u64>())
+            },
+            Part::Two => unimplemented!()
+        }
+    }
+}
